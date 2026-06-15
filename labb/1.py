1fd4e9aea26311a465b7a1c063d8b172f383c094
Aakritii01 <vc81it01@vedascollege.edu.np>
2026-06-15 10:17:47 +0545

    lab2

--- labb/1.py
+++ labb/1.py
@@ -1,32 +1,35 @@
-import random
-
-def get_user_guess():
-    return int(input("Guess a number between 1 and 100: "))
-
-def check_guess(guess, secret):
-    if guess < secret:
-        print("Too low!")
-        return False
-    elif guess > secret:
-        print("Too high!")
-        return False
+def simple_reflex_agent(location, status):
+    # location: 'A' or 'B', status: 'Dirty' or 'Clean'
+    if status == "Dirty":
+        return "Suck"
+    elif location == "A":
+        return "Move to B"
+    elif location == "B":
+        return "Move to A"
     else:
-        print("Correct! You guessed it!")
-        return True
+        return "NoOp"
 
-def play_game():
-    secret_number = random.randint(1, 100)
-    attempts = 0
-    guessed = False
 
-    print("Welcome to the Number Guessing Game!")
+print("********** Simple Reflex Agent **********\n")
+print("Program by: [Aakriti Chitrakar]")
+print("Roll no: [01]\n")
 
-    while not guessed:
-        guess = get_user_guess()
-        attempts += 1
-        guessed = check_guess(guess, secret_number)
 
-    print(f"You took {attempts} attempts.")
+print("\nThe percept sequence is (A, Dirty)")
+action = simple_reflex_agent("A", "Dirty")
+print("Action:", action)
 
-# Start the game
-play_game()
+print("\nThe percept sequence is (A, Clean)")
+action = simple_reflex_agent("A", "Clean")
+print("Action:", action)
+
+
+print("\nThe percept sequence is (B, Dirty)")
+action = simple_reflex_agent("B", "Dirty")
+print("Action:", action)
+
+
+print("\nThe percept sequence is (B, Clean)")
+action = simple_reflex_agent("B", "Clean")
+print("Action:", action)
+
